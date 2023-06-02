544cb5e8575ab8fda790ad322c455683209f85b9
Natalia Gill <sngill.ca@gmail.com>
2023-06-01 19:00:18 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const hamburgerClose = document.querySelector(".hamburger-close");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  hamburgerClose.classList.toggle("active");
+  navList.classList.toggle("active");
+});
+
+navList.childNodes.forEach((l) => {
+  l.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    hamburgerClose.classList.remove("active");
+    navList.classList.remove("active");
+  });
+});
+
+// Scroll animation library
+ScrollReveal().reveal(".section-container", {
+  easing: "ease-in",
+  duration: 800,
+  reset: true,
+});
